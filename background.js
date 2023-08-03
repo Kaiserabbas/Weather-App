b829672e85e3a6503228e849ea260dfe4a09bc84
Qaisar Abbas <kayser.abbas@gmail.com>
2023-08-03 15:56:21 +0500

    Create background.js
    Add code for using dynamic background images from Unsplash.com

---
+++ background.js
@@ -0,0 +1,17 @@
+const unsplashAccessKey = 'XgK8K70K3KIJdzJUlEQRm-GgqODayfcdNLPnOtjo81o';
+
+function setBackgroundFromUnsplash(city) {
+  const query = encodeURIComponent(`${city} city`); // Append "city" to the query
+  const unsplashUrl = `https://api.unsplash.com/photos/random?query=${query}&client_id=${unsplashAccessKey}`;
+
+  fetch(unsplashUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const imageUrl = data.urls.regular;
+      document.body.style.backgroundImage = `url(${imageUrl})`;
+    })
+    .catch((error) => {
+      console.error('Error fetching background image:', error);
+    });
+}
+setBackgroundFromUnsplash();
